ea3eb2cf7dce85b57dd0a38a3356b0c802e392d0
namin <tnddl032@gmail.com>
2024-10-07 18:24:56 +0900

    programmers 43165

---
+++ programmers/43165.js
@@ -0,0 +1,20 @@
+function solution(numbers, target) {
+  let result = 0;
+
+  function dfs(index, sum) {
+    if (index == numbers.length) {
+      if (sum === target) {
+        result++;
+      }
+
+      return;
+    }
+
+    dfs(index + 1, sum + numbers[index]);
+    dfs(index + 1, sum - numbers[index]);
+  }
+
+  dfs(0, 0);
+
+  return result;
+}
